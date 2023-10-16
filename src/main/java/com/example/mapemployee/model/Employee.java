f3e3a1b09eb6ac2e4c7dd2344de7f1d93af72b46
Олег <olegcelysev778@gmail.com>
2023-10-16 16:40:42 +0300

    тест

---
+++ src/main/java/com/example/mapemployee/model/Employee.java
@@ -0,0 +1,75 @@
+package com.example.mapemployee.model;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Objects;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private Integer department;
+    private double salary;
+
+    public Employee(String firstName, String lastName, Integer department, double salary) {
+        this.firstName = StringUtils.capitalize(firstName.toLowerCase());
+        this.lastName = StringUtils.capitalize(lastName.toLowerCase());
+        this.department = department;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public Integer getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Integer department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/java/com/example/mapemployee/MapemployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapemployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
