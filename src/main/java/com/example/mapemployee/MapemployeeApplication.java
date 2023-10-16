2fb781a19c5164e49ec5e202b746a2db1d964338
Олег <olegcelysev778@gmail.com>
2023-10-16 13:09:49 +0300

    коммит

---
+++ src/main/java/com/example/mapemployee/MapemployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapemployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapemployeeApplication.class, args);
+	}
+
+}
